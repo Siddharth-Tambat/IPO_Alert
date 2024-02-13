9e881b00245f4c4012aeecbb9e95ac4a9f93a53e
Siddharth Tambat <83575017+Siddharth-Tambat@users.noreply.github.com>
2024-02-13 15:08:14 +0530

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-GROUP_ID = os.getenv('GROUP_ID')
+CHANNEL_ID = os.getenv('CHANNEL_ID')
 bot = telebot.TeleBot(os.getenv('API_KEY'))
 
 
@@ -13,5 +13,5 @@ def ipo_alert(message, link):
 
     keyboard = telebot.types.InlineKeyboardMarkup()
     keyboard.add(ipo_button)
-    bot.send_message(chat_id=GROUP_ID, text=message, reply_markup=keyboard)
+    bot.send_message(chat_id=CHANNEL_ID, text=message, reply_markup=keyboard)
     
